5591fcbecad0a3338eb4be5227a336861694cd2e
Mazin Saeed <mazinsaeed@Mazins-MacBook-Air.local>
2021-06-29 16:41:10 -0700

    changes

--- test.py
+++ test.py
@@ -1,2 +1,18 @@
 print("hello world")
 print("testing")
+
+
+name = "Lebron" 
+age = 35
+salary = 37.44
+plays_basketball = True
+jersery_number = "23"
+
+car_name = "Mercedes"
+x = 50
+
+print(type(name))
+print(type(age))
+print(type(salary))
+print(type(plays_basketball))
+print(type(jersery_number))
